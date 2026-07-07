b544ad45b759e19a6b0e165b9e33e718971825a9
JAYBLA <jayblaenterprises@gmail.com>
2026-07-07 12:14:31 +0300

    quotation templating

--- quotations/views.py
+++ quotations/views.py
@@ -1,24 +1,19 @@
 import json
-from io import BytesIO
 from pathlib import Path
-
+from django import forms
 from django.contrib.staticfiles import finders
 from django.db import transaction
 from django.db.models import ProtectedError
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import get_object_or_404, render
 from django.template.loader import render_to_string
-from django.templatetags.static import static
 from django.utils.text import slugify
-
-from pypdf import PdfReader, PdfWriter
 from weasyprint import HTML
-
 from customers.models import Customer
 from products.models import Product
-
 from .forms import QuotationForm, QuotationItemFormSet
 from .models import Quotation
+from utils import apply_document_backgrounds
 
 
 # =====================================================
@@ -555,237 +550,6 @@ def product_state(
     )
 
 # =====================================================
-# CREATE A4 BACKGROUND PDF
-# =====================================================
-
-
-def create_background_pdf(image_uri):
-
-    html = f"""
-    <!doctype html>
-
-    <html>
-
-    <head>
-
-        <meta charset="utf-8">
-
-
-        <style>
-
-
-            @page {{
-
-                size: A4 portrait;
-
-                margin: 0;
-
-            }}
-
-
-            html,
-            body {{
-
-                margin: 0;
-
-                padding: 0;
-
-                width: 210mm;
-
-                height: 297mm;
-
-            }}
-
-
-            img {{
-
-                display: block;
-
-                width: 210mm;
-
-                height: 297mm;
-
-            }}
-
-
-        </style>
-
-    </head>
-
-
-    <body>
-
-
-        <img src="{image_uri}">
-
-
-    </body>
-
-    </html>
-    """
-
-
-    return HTML(
-        string=html,
-    ).write_pdf()
-
-# =====================================================
-# APPLY PAGE BACKGROUNDS
-# =====================================================
-
-
-def apply_quotation_backgrounds(
-    content_pdf,
-    header_bg,
-    footer_bg,
-    single_bg,
-):
-
-    # =========================================
-    # READ CONTENT PDF
-    # =========================================
-
-    content_reader = PdfReader(
-        BytesIO(content_pdf)
-    )
-
-
-    page_count = len(
-        content_reader.pages
-    )
-
-
-    # =========================================
-    # CREATE BACKGROUND PDFS
-    # =========================================
-
-    header_pdf = create_background_pdf(
-        header_bg
-    )
-
-
-    footer_pdf = create_background_pdf(
-        footer_bg
-    )
-
-
-    single_pdf = create_background_pdf(
-        single_bg
-    )
-
-
-    # =========================================
-    # READ BACKGROUND PDFS
-    # =========================================
-
-    header_reader = PdfReader(
-        BytesIO(header_pdf)
-    )
-
-
-    footer_reader = PdfReader(
-        BytesIO(footer_pdf)
-    )
-
-
-    single_reader = PdfReader(
-        BytesIO(single_pdf)
-    )
-
-
-    # =========================================
-    # PDF WRITER
-    # =========================================
-
-    writer = PdfWriter()
-
-
-    # =========================================
-    # PROCESS EACH PAGE
-    # =========================================
-
-    for index, content_page in enumerate(
-        content_reader.pages
-    ):
-
-        # =====================================
-        # SINGLE PAGE
-        # =====================================
-
-        if page_count == 1:
-
-            background_page = (
-                single_reader.pages[0]
-            )
-
-
-        # =====================================
-        # FIRST PAGE
-        # =====================================
-
-        elif index == 0:
-
-            background_page = (
-                header_reader.pages[0]
-            )
-
-
-        # =====================================
-        # LAST PAGE
-        # =====================================
-
-        elif index == page_count - 1:
-
-            background_page = (
-                footer_reader.pages[0]
-            )
-
-
-        # =====================================
-        # MIDDLE PAGE
-        # =====================================
-
-        else:
-
-            writer.add_page(
-                content_page
-            )
-
-            continue
-
-
-        # =====================================
-        # BACKGROUND FIRST
-        # CONTENT SECOND
-        # =====================================
-
-        background_page.merge_page(
-            content_page
-        )
-
-
-        writer.add_page(
-            background_page
-        )
-
-
-    # =========================================
-    # WRITE FINAL PDF
-    # =========================================
-
-    output = BytesIO()
-
-
-    writer.write(
-        output
-    )
-
-
-    output.seek(0)
-
-
-    return output.getvalue()
-# =====================================================
 # DOWNLOAD QUOTATION PDF
 # =====================================================
 
@@ -971,7 +735,7 @@ def download_quotation_pdf(request, pk):
     # APPLY PAGE BACKGROUNDS
     # =========================================
 
-    pdf = apply_quotation_backgrounds(
+    pdf = apply_document_backgrounds(
         content_pdf=content_pdf,
         header_bg=header_bg,
         footer_bg=footer_bg,
@@ -1024,7 +788,7 @@ def download_quotation_pdf(request, pk):
 
     response["Content-Disposition"] = (
 
-        "inline; "
+        "attachment; "
 
         f'filename="{filename}"'
 
--- utils.py
+++ utils.py
@@ -1,7 +1,10 @@
 from io import BytesIO
+
 from django.http import HttpResponse
 from django.template.loader import get_template
 
+from pypdf import PdfReader, PdfWriter
+from weasyprint import HTML
 from xhtml2pdf import pisa
 
 def render_to_pdf(template_src, context_dict={}):
@@ -12,3 +15,235 @@ def render_to_pdf(template_src, context_dict={}):
     if not pdf.err:
         return HttpResponse(result.getvalue(), content_type='application/pdf')
     return None
+
+# =====================================================
+# CREATE A4 BACKGROUND PDF
+# =====================================================
+
+
+def create_background_pdf(image_uri):
+
+    html = f"""
+    <!doctype html>
+
+    <html>
+
+    <head>
+
+        <meta charset="utf-8">
+
+
+        <style>
+
+
+            @page {{
+
+                size: A4 portrait;
+
+                margin: 0;
+
+            }}
+
+
+            html,
+            body {{
+
+                margin: 0;
+
+                padding: 0;
+
+                width: 210mm;
+
+                height: 297mm;
+
+            }}
+
+
+            img {{
+
+                display: block;
+
+                width: 210mm;
+
+                height: 297mm;
+
+            }}
+
+
+        </style>
+
+    </head>
+
+
+    <body>
+
+
+        <img src="{image_uri}">
+
+
+    </body>
+
+    </html>
+    """
+
+
+    return HTML(
+        string=html,
+    ).write_pdf()
+
+# =====================================================
+# APPLY PAGE BACKGROUNDS
+# =====================================================
+
+
+def apply_document_backgrounds(
+    content_pdf,
+    header_bg,
+    footer_bg,
+    single_bg,
+):
+
+    # =========================================
+    # READ CONTENT PDF
+    # =========================================
+
+    content_reader = PdfReader(
+        BytesIO(content_pdf)
+    )
+
+
+    page_count = len(
+        content_reader.pages
+    )
+
+
+    # =========================================
+    # CREATE BACKGROUND PDFS
+    # =========================================
+
+    header_pdf = create_background_pdf(
+        header_bg
+    )
+
+
+    footer_pdf = create_background_pdf(
+        footer_bg
+    )
+
+
+    single_pdf = create_background_pdf(
+        single_bg
+    )
+
+
+    # =========================================
+    # READ BACKGROUND PDFS
+    # =========================================
+
+    header_reader = PdfReader(
+        BytesIO(header_pdf)
+    )
+
+
+    footer_reader = PdfReader(
+        BytesIO(footer_pdf)
+    )
+
+
+    single_reader = PdfReader(
+        BytesIO(single_pdf)
+    )
+
+
+    # =========================================
+    # PDF WRITER
+    # =========================================
+
+    writer = PdfWriter()
+
+
+    # =========================================
+    # PROCESS EACH PAGE
+    # =========================================
+
+    for index, content_page in enumerate(
+        content_reader.pages
+    ):
+
+        # =====================================
+        # SINGLE PAGE
+        # =====================================
+
+        if page_count == 1:
+
+            background_page = (
+                single_reader.pages[0]
+            )
+
+
+        # =====================================
+        # FIRST PAGE
+        # =====================================
+
+        elif index == 0:
+
+            background_page = (
+                header_reader.pages[0]
+            )
+
+
+        # =====================================
+        # LAST PAGE
+        # =====================================
+
+        elif index == page_count - 1:
+
+            background_page = (
+                footer_reader.pages[0]
+            )
+
+
+        # =====================================
+        # MIDDLE PAGE
+        # =====================================
+
+        else:
+
+            writer.add_page(
+                content_page
+            )
+
+            continue
+
+
+        # =====================================
+        # BACKGROUND FIRST
+        # CONTENT SECOND
+        # =====================================
+
+        background_page.merge_page(
+            content_page
+        )
+
+
+        writer.add_page(
+            background_page
+        )
+
+
+    # =========================================
+    # WRITE FINAL PDF
+    # =========================================
+
+    output = BytesIO()
+
+
+    writer.write(
+        output
+    )
+
+
+    output.seek(0)
+
+
+    return output.getvalue()
