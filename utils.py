e5d284062eb24c46522fe4d9657b437ca965615d
JAYBLA <jayblaenterprises@gmail.com>
2026-07-07 16:36:56 +0300

    PyPDF2

--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ from io import BytesIO
 from django.http import HttpResponse
 from django.template.loader import get_template
 
-from pypdf import PdfReader, PdfWriter
+from PyPDF2 import PdfReader, PdfWriter
 from weasyprint import HTML
 from xhtml2pdf import pisa
 
